95ece84c624ebc6637944f471c91ac043b6ca1eb
David Clutter <cluttered.code@gmail.com>
2025-03-09 13:01:47 -0700

    add resoures to start rewrite

---
+++ addons/health_hitbox_hurtbox/resources/modifier.gd
@@ -0,0 +1,19 @@
+class_name HealthModifier extends Resource
+
+@export var incrementer: int = 0
+@export var multiplier: float = 1.0
+
+@export var convert_affect: Health.Affect = Health.Affect.NONE
+@export var convert_type: HealthActionType.Enum = HealthActionType.Enum.NONE
+
+
+func _init(
+	incrementer: int = 1,
+	multiplier: float = 1.0,
+	convert_affect: Health.Affect = Health.Affect.NONE,
+	convert_type: HealthActionType.Enum = HealthActionType.Enum.NONE
+) -> void:
+	self.incrementer = incrementer
+	self.multiplier = multiplier
+	self.convert_affect = convert_affect
+	self.convert_type = convert_type
