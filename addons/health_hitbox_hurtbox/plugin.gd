8e509194a19676e7cbbf0f3a32d60d17ae0f9a9b
David Clutter <cluttered.code@gmail.com>
2025-03-09 21:23:46 -0700

    fix tutorial

--- addons/health_hitbox_hurtbox/plugin.gd
+++ addons/health_hitbox_hurtbox/plugin.gd
@@ -27,10 +27,10 @@ func _enter_tree() -> void:
 		add_custom_type("HitScan2D", "RayCast2D",
 			load(addon_path % "2d/hit_scan_2d/hit_scan_2d.gd"),
 			load(addon_path % "2d/hit_scan_2d/hit_scan_2d.svg"))
-	if FileAccess.file_exists(addon_path % "2d/hurt_box_2d/hurt_box_2d.gd"):
+	if FileAccess.file_exists(addon_path % "2d/hurt_box_2d/basic_hurt_box_2d.gd"):
 		hurt_box_2d_added = true
 		add_custom_type("BasicHurtBox2D", "Area2D",
-			load(addon_path % "2d/hurt_box_2d/hurt_box_2d.gd"),
+			load(addon_path % "2d/hurt_box_2d/basic_hurt_box_2d.gd"),
 			load(addon_path % "2d/hurt_box_2d/hurt_box_2d.svg"))
 	
 	# Load 3D components
@@ -44,10 +44,10 @@ func _enter_tree() -> void:
 		add_custom_type("HitScan3D", "RayCast3D",
 			load(addon_path % "3d/hit_scan_3d/hit_scan_3d.gd"),
 			load(addon_path % "3d/hit_scan_3d/hit_scan_3d.svg"))
-	if FileAccess.file_exists(addon_path % "3d/hurt_box_3d/hurt_box_3d.gd"):
+	if FileAccess.file_exists(addon_path % "3d/hurt_box_3d/basic_hurt_box_3d.gd"):
 		hurt_box_3d_added = true
 		add_custom_type("BasicHurtBox3D", "Area3D",
-			load(addon_path % "3d/hurt_box_3d/hurt_box_3d.gd"),
+			load(addon_path % "3d/hurt_box_3d/basic_hurt_box_3d.gd"),
 			load(addon_path % "3d/hurt_box_3d/hurt_box_3d.svg"))
 
 
