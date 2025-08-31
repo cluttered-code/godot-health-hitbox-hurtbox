ceba795cba7f954625bd1682b820bcec5facab18
David Clutter <cluttered.code@gmail.com>
2025-08-30 22:23:39 -0700

    test updates

--- test/advanced_hit_box_2d_test.gd
+++ test/advanced_hit_box_2d_test.gd
@@ -1,15 +1,15 @@
-class_name HitBox2DTest extends GdUnitTestSuite
+class_name AdvancedHitBox2DTest extends GdUnitTestSuite
 @warning_ignore('unused_parameter')
 @warning_ignore('return_value_discarded')
 
 const SIG_HURT_BOX_ENTERED := "hurt_box_entered"
-const SIG_ACTION_APPLIED := "action_applied"
+const SIG_ACTIONS_APPLIED := "actions_applied"
 const SIG_HIT_BOX_ENTERED := "hit_box_entered"
 const SIG_UNKNOWN_AREA_ENTERED := "unknown_area_entered"
 
 
 var mock_hurt_box: HurtBox2D
-var hit_box: HitBox2D
+var hit_box: AdvancedHitBox2D
 var action: HealthAction
 var signals: Object
 
@@ -17,7 +17,7 @@ var signals: Object
 func before_test() -> void:
 	mock_hurt_box = auto_free(mock(HurtBox2D))
 
-	hit_box = auto_free(HitBox2D.new())
+	hit_box = auto_free(AdvancedHitBox2D.new())
 	add_child(hit_box)
 
 	action = auto_free(HealthAction.new(Health.Affect.DAMAGE, HealthActionType.Enum.KINETIC, 10))
@@ -32,7 +32,7 @@ func test_on_area_entered_hurt_box() -> void:
 	verify(mock_hurt_box, 1).apply_all_actions([HealthActionMatcher.new(action)])
 	
 	await assert_signal(signals).is_emitted(SIG_HURT_BOX_ENTERED, [mock_hurt_box])
-	await assert_signal(signals).is_emitted(SIG_ACTION_APPLIED, [mock_hurt_box])
+	await assert_signal(signals).is_emitted(SIG_ACTIONS_APPLIED, [mock_hurt_box])
 
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
@@ -46,7 +46,7 @@ func test_on_area_entered_hit_box() -> void:
 	await assert_signal(signals).is_emitted(SIG_HIT_BOX_ENTERED, [hit_box])
 
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 
 
@@ -60,7 +60,7 @@ func test_on_area_entered_ignore() -> void:
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
 
 
 func test_on_area_entered_area() -> void:
@@ -74,4 +74,4 @@ func test_on_area_entered_area() -> void:
 	
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
--- test/advanced_hit_scan_2d_test.gd
+++ test/advanced_hit_scan_2d_test.gd
@@ -1,21 +1,21 @@
-class_name HitScan2DTest extends GdUnitTestSuite
+class_name AdvancedHitScan2DTest extends GdUnitTestSuite
 @warning_ignore('unused_parameter')
 @warning_ignore('return_value_discarded')
 
 const SIG_HURT_BOX_ENTERED := "hurt_box_entered"
-const SIG_ACTION_APPLIED := "action_applied"
+const SIG_ACTIONS_APPLIED := "actions_applied"
 const SIG_HIT_BOX_ENTERED := "hit_box_entered"
 const SIG_UNKNOWN_AREA_ENTERED := "unknown_area_entered"
 
 var mock_hurt_box: HurtBox2D
-var hit_scan: HitScan2D
+var hit_scan: AdvancedHitScan2D
 var action: HealthAction
 var signals: Object
 
 
 func before_test() -> void:
 	mock_hurt_box = mock(HurtBox2D)
-	hit_scan = auto_free(HitScan2D.new())
+	hit_scan = auto_free(AdvancedHitScan2D.new())
 	add_child(hit_scan)
 
 	action = auto_free(HealthAction.new(Health.Affect.DAMAGE, HealthActionType.Enum.KINETIC, 10))
@@ -25,14 +25,14 @@ func before_test() -> void:
 
 
 func test_fire_hurt_box() -> void:
-	hit_scan._collider = mock_hurt_box
+	hit_scan._test_collider = mock_hurt_box
 
 	hit_scan.fire()
 
 	verify(mock_hurt_box, 1).apply_all_actions([HealthActionMatcher.new(action)])
 	
 	await assert_signal(signals).is_emitted(SIG_HURT_BOX_ENTERED, [mock_hurt_box])
-	await assert_signal(signals).is_emitted(SIG_ACTION_APPLIED, [mock_hurt_box])
+	await assert_signal(signals).is_emitted(SIG_ACTIONS_APPLIED, [mock_hurt_box])
 
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
@@ -40,7 +40,7 @@ func test_fire_hurt_box() -> void:
 
 func test_fire_hit_box() -> void:
 	var hit_box: HitBox2D = auto_free(HitBox2D.new())
-	hit_scan._collider = hit_box
+	hit_scan._test_collider = hit_box
 
 	hit_scan.fire()
 
@@ -49,14 +49,14 @@ func test_fire_hit_box() -> void:
 	await assert_signal(signals).is_emitted(SIG_HIT_BOX_ENTERED, [hit_box])
 	
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 
 
 func test_fire_hit_box_ignore() -> void:
 	var hit_box: HitBox2D = auto_free(HitBox2D.new())
 	hit_box.ignore_collisions = true
-	hit_scan._collider = hit_box
+	hit_scan._test_collider = hit_box
 
 	hit_scan.fire()
 
@@ -64,13 +64,13 @@ func test_fire_hit_box_ignore() -> void:
 	
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_UNKNOWN_AREA_ENTERED, [any()])
 
 
 func test_fire_area() -> void:
 	var area: Area2D = auto_free(Area2D.new())
-	hit_scan._collider = area
+	hit_scan._test_collider = area
 
 	hit_scan.fire()
 
@@ -80,4 +80,4 @@ func test_fire_area() -> void:
 	
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HIT_BOX_ENTERED, [any()])
 	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_HURT_BOX_ENTERED, [any()])
-	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTION_APPLIED, [any()])
+	await assert_signal(signals).wait_until(50).is_not_emitted(SIG_ACTIONS_APPLIED, [any()])
