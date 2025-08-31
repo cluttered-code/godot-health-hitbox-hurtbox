9bef13f161a990b7c5c8dabb3974874cc02982a3
David Clutter <cluttered.code@gmail.com>
2025-08-30 22:30:21 -0700

    fix basic_hit_box_2d tests

--- test/basic_hit_box_2d_test.gd
+++ test/basic_hit_box_2d_test.gd
@@ -1,35 +1,37 @@
-class_name BasicHitBox2DTest extends GdUnitTestSuite
+class_name BasicHitScan2DTest extends GdUnitTestSuite
 @warning_ignore('unused_parameter')
 @warning_ignore('return_value_discarded')
 
 var mock_hurt_box: BasicHurtBox2D
-var hit_box: BasicHitBox2D
+var hit_scan: BasicHitScan2D
+var signals: Object
 
 
 func before_test() -> void:
-	mock_hurt_box = auto_free(mock(BasicHurtBox2D))
-	hit_box = auto_free(BasicHitBox2D.new())
-	add_child(hit_box)
+	mock_hurt_box = mock(BasicHurtBox2D)
+	hit_scan = auto_free(BasicHitScan2D.new())
+	add_child(hit_scan)
+	signals = monitor_signals(hit_scan)
 
 
 func test_defaults() -> void:
 	var action := HealthAction.new()
-	assert_int(hit_box._actions.size()).is_equal(1)
-	assert_object(hit_box._actions[0]).is_equal(HealthActionMatcher.new(action))
+	assert_int(hit_scan._actions.size()).is_equal(1)
+	assert_object(hit_scan._actions[0]).is_equal(HealthActionMatcher.new(action))
 
 
 func test_affect() -> void:
-	hit_box.affect = Health.Affect.HEAL
+	hit_scan.affect = Health.Affect.HEAL
 
 	var action := HealthAction.new(Health.Affect.HEAL, HealthActionType.Enum.MEDICINE)
-	assert_int(hit_box._actions.size()).is_equal(1)
-	assert_object(hit_box._actions[0]).is_equal(HealthActionMatcher.new(action))
+	assert_int(hit_scan._actions.size()).is_equal(1)
+	assert_object(hit_scan._actions[0]).is_equal(HealthActionMatcher.new(action))
 
 
 func test_amount() -> void:
-	hit_box.amount = 25
+	hit_scan.amount = 25
 
 	var action := HealthAction.new()
 	action.amount = 25
-	assert_int(hit_box._actions.size()).is_equal(1)
-	assert_object(hit_box._actions[0]).is_equal(HealthActionMatcher.new(action))
+	assert_int(hit_scan._actions.size()).is_equal(1)
+	assert_object(hit_scan._actions[0]).is_equal(HealthActionMatcher.new(action))
